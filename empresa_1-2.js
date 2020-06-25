aaf0b765a11eeb173b46f23cea3990bc46c6fff8
Luciano Barreto <contato.lucianobarreto@gmail.com>
2020-06-25 15:32:08 -0300

    Objetos e Vetores

---
+++ empresa_1-2.js
@@ -0,0 +1,13 @@
+// Junção
+
+const empresa = {
+    nome: 'Rocketseat',
+    cor: 'Roxo',
+    foco: 'Programação',
+    endereco: {
+      rua: 'Airton Senna',
+      numero: 745,
+    }
+  }
+  
+  console.log(`A empresa ${empresa.nome} está localizada em Rua ${empresa.endereco.rua}, ${empresa.endereco.numero}`);
